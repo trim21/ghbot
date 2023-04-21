42ec8694994111c222d42ee456338de6ffdd6e75
Trim21 <trim21.me@gmail.com>
2023-04-21 08:37:01 +0800

    chore: log request error

--- lib/server.ts
+++ lib/server.ts
@@ -3,6 +3,8 @@ import * as http from 'node:http';
 import { Octokit } from '@octokit/rest';
 import { createNodeMiddleware, Webhooks } from '@octokit/webhooks';
 
+import { logger } from '@app/lib/logger.ts';
+
 export function createServer(): http.Server {
   const octokit = new Octokit({ auth: process.env.GH_TOKEN });
 
@@ -30,15 +32,22 @@ export function createServer(): http.Server {
       return;
     }
 
-    await octokit.request('POST /repos/{owner}/{repo}/actions/workflows/{workflow_id}/dispatches', {
-      owner: 'trim21',
-      repo: 'actions-cron',
-      workflow_id: 'mirror-docker.yaml',
-      ref: 'master',
-      headers: {
-        'X-GitHub-Api-Version': '2022-11-28',
+    const res = await octokit.request(
+      'POST /repos/{owner}/{repo}/actions/workflows/{workflow_id}/dispatches',
+      {
+        owner: 'trim21',
+        repo: 'actions-cron',
+        workflow_id: 'mirror-docker.yaml',
+        ref: 'master',
+        headers: {
+          'X-GitHub-Api-Version': '2022-11-28',
+        },
       },
-    });
+    );
+
+    if (res.status !== 204) {
+      logger.error(`failed to handle event ${ctx.id}:\n`, res.data);
+    }
   });
 
   // eslint-disable-next-line @typescript-eslint/no-misused-promises
