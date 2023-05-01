6c502e2d38308f4126eccc530fda58ea60b9bb91
Trim21 <trim21.me@gmail.com>
2023-05-01 23:24:44 +0800

    chore: remove prettier-plugin-sh

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,9 +1,8 @@
 // lint-staged.config.js
 export default {
-  '*.{md,html,json,cts,cjs,mjs,js,yml,yaml,liquid,dockerfile}': 'prettier -w',
+  '*.{md,html,json,cts,cjs,mjs,js,yml,yaml,liquid}': 'prettier -w',
   '*.{cts,cjs,mjs,js}': 'eslint --fix',
   '**/pre-commit': 'prettier -w',
-  '.{git,docker}ignore': 'prettier -w',
   '*.ts': [
     'prettier -w',
     () => 'tsc -p tsconfig.json --pretty --noEmit', // do not pass filenames
